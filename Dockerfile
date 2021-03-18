01f6a9884fecbec36d2e81e5d8636a194d10e1cc
anthony.santillan <anthony.santillan@itriad.org.br>
2021-03-18 11:58:15 -0400

    Commit first

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# Pull base image
+FROM python:3.8-slim
+
+# Set environment varibles
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /code/
+
+# Install dependencies
+
+COPY Pipfile Pipfile.lock /code/
+
+RUN apt-get update -qq && \
+    apt-get clean && \
+    rm -rf /var/cache/apk/* && \
+    pip install --upgrade pip==20.2.3 --no-cache-dir
+
+RUN pip install pipenv --no-cache-dir && \
+    pipenv install --system --ignore-pipfile --deploy
+
+COPY . /code/
+
+EXPOSE 8000
+
+#CMD [ "uvicorn", "main:app", "--reload", "--workers", "1",  "--host",  "0.0.0.0", "--port", "8000"  ]
---
+++ main.py
@@ -0,0 +1,43 @@
+from typing import List
+
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from app import models, schemas
+from app.crud_user import get_user_by_email, get_users, get_user
+from app.database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
